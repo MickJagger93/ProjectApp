2c6e12589dcb70baee234aa2e2cc4e3378af2074
JaggerMQ <quevedo993mi@gmail.com>
2026-03-31 17:52:51 +0530

    database

--- projectapp/projectapp/settings.py
+++ projectapp/projectapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,16 +75,10 @@ WSGI_APPLICATION = 'projectapp.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'CxscStkqBfkOpPMLdeKrKlQZSLJTzghm',
-        'HOST': '@mainline.proxy.rlwy.net',
-        'PORT': '33308',
-    }
+    'default': dj_database_url.config(
+        default='DATABASE_URL'
+    )
 }
-
 
 
 # Password validation
